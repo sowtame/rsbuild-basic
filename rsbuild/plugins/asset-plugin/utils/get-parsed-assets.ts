950e155dbc115a72a9992a2ab4f7511dbc3e729c
u_m1mkt <sowtame@gmail.com>
2024-04-09 14:57:32 +0300

    add proxy prefix temp

--- rsbuild/plugins/asset-plugin/utils/get-parsed-assets.ts
+++ rsbuild/plugins/asset-plugin/utils/get-parsed-assets.ts
@@ -8,17 +8,20 @@ export type AppManifest = {
   js: AppManifestFile[]
 }
 
+const domain = 'http://localhost:8080/'
+
 export const getParsedAssets = (assets: JsStatsChunkGroupAsset[]) => {
   return assets.reduce<AppManifest>(
     (acc, asset) => {
+      const path = `${domain}${asset.name}`
       if (asset.name.includes('css')) {
         acc.css.push({
-          path: asset.name,
+          path,
         })
       }
       if (asset.name.includes('js')) {
         acc.js.push({
-          path: asset.name,
+          path,
         })
       }
 
